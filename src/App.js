8a64e395da879d732dc14addcbb8cbdf4a423c89
luiz <phellipe501@gmail.com>
2020-09-24 22:49:53 -0300

    Faltando filtros

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import Index from './Pages/Index';
+import CountryPage from './Pages/CountryPage';
 import './App.css';
+import Header from './Components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      classes: '',
+      dark: true,
+    }
+  }
+  render() {
+    return (
+      <div className={this.state.classes}>
+      <Header handleChange={() => {
+        if(this.state.dark) {
+          this.setState(({classes: 'dark-mode', dark: false}))
+        } else {
+          this.setState(({classes: '', dark: true}))
+        }
+      }}/>
+      <Switch>
+        <Route path="/countries/:id" render={(props) => <CountryPage {...props} />} />
+        <Route exact path="/" component={Index} />
+      </Switch>
     </div>
-  );
+    )
+  } 
 }
 
 export default App;
